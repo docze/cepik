4797a24a5dbd49e7f2e8fdc07347268d16fc10fd
SimonPaperman <szymonpapierzewski@gmail.com>
2017-10-26 11:04:59 +0200

    init

---
+++ src/main/java/com/mwsi/cepik/CepikApplication.java
@@ -0,0 +1,12 @@
+package com.mwsi.cepik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CepikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CepikApplication.class, args);
+	}
+}
---
+++ src/test/java/com/mwsi/cepik/CepikApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mwsi.cepik;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CepikApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
