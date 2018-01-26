46d12a21f23950d863d062708b3c0b9d626abac7
fcmichael <michael.1@wp.pl>
2018-01-26 23:09:18 +0100

    Update SIGN_UP_URL

--- src/main/java/com/mwsi/cepik/security/SecurityConstants.java
+++ src/main/java/com/mwsi/cepik/security/SecurityConstants.java
@@ -10,5 +10,5 @@ class SecurityConstants {
     static final long EXPIRATION_TIME = 864_000_000; // 10 days
     static final String TOKEN_PREFIX = "Token ";
     static final String HEADER_STRING = "Authorization";
-    static final String SIGN_UP_URL = "/users/registration";
+    static final String SIGN_UP_URL = "/users";
 }
